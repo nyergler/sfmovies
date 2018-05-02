23443abd7660308bf9fec0974374d15154a7b518
Nathan Yergler <nathan@yergler.net>
2018-05-02 14:43:45 -0700

    Add support for filtering movies (#4)
    * feat(movies): Add controller support for finding movies.
    
    * feat(movies) Validate movie filter request payloads
    
    * feat(movies) Add /movies filter endpoint

--- test/plugins/features/movies/index.test.js
+++ test/plugins/features/movies/index.test.js
@@ -1,8 +1,15 @@
 'use strict';
 
+const knex = require('../../../../lib/libraries/knex');
+
+const Controller  = require('../../../../lib/plugins/features/movies/controller');
 const MovieServer = require('../../../../lib/server');
 
 describe('movieserver integration', () => {
+
+  beforeEach(() => {
+    return knex.truncate('movies');
+  });
 
   describe('create', () => {
 
@@ -15,6 +22,39 @@ describe('movieserver integration', () => {
       .then((response) => {
         expect(response.statusCode).to.eql(200);
         expect(response.result.object).to.eql('movie');
+      });
+    });
+
+  });
+
+  describe('filter', () => {
+
+    beforeEach(() => {
+      return Promise.all([
+        Controller.create({
+          title: 'Bullitt',
+          release_year: 1968
+        }),
+        Controller.create({
+          title: 'The Rock',
+          release_year: 1996
+        }),
+        Controller.create({
+          title: 'Star Trek IV: The Voyage Home',
+          release_year: 1986
+        })
+      ]);
+    });
+
+    it('can filter movies based on title and year', () => {
+      return MovieServer.inject({
+        url: '/movies?title_contains=Rock&released_after=1900&released_before=1997'
+      })
+      .then((response) => {
+        expect(response.statusCode).to.eql(200);
+        expect(response.result.length).to.eql(1);
+        expect(response.result[0].object).to.eql('movie');
+        expect(response.result[0].title).to.eql('The Rock');
       });
     });
 
